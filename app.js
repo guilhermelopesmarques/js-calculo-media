0f28e32bf5d5825aff525593cac53d4c84ccb671
guilherme <112526329+guilhermelopesmarques@users.noreply.github.com>
2022-10-18 21:03:01 -0300

    js-calculo-media
    calculo-de-media

---
+++ app.js
@@ -0,0 +1,14 @@
+// console. log("olá, estou funcionando")
+// console. log(12*6)
+// console. log(60/6)
+
+let nome = "guilherme"
+let nota1 = 9.3
+let nota2 = 7.2
+let nota3 = 4.5
+let nota4 = 2.33
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+let mediafixada = media.toFixed(1)
+
+console.log("olá " + nome)
+console.log("sua media foi:" + mediafixada)
